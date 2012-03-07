2dd7735c797f977f6ae587d9055eb7db1ede221e
Mike <mike@citrin.ch>
2012-03-07 23:47:16 +0100

    cleaned up api, added netzke_submit

--- lib/netzke/communitypack/group_explorer.rb
+++ lib/netzke/communitypack/group_explorer.rb
@@ -8,30 +8,63 @@ module Netzke
       def js_config
         group_config = configuration[:group_config] ? configuration[:group_config] : {}
         item_config = configuration[:item_config] ? configuration[:item_config] : {}
+        selected_ids = configuration[:selected_ids].call(the_record)
         groups = []
-        grp = 0; configuration[:get_records].call.each do |c|
-          if configuration[:get_relation].call(c).id != grp
-            @mymodel_group = {
-              :xtype => 'checkboxgroup',
-              :fieldLabel => configuration[:get_relation].call(c).to_s + " " + configuration[:get_relation].call(c).activities.count.to_s,
-              :cls => (grp % 2) == 0  ? 'x-check-group-alt' : '' ,
-              :columns => 7,
-              :items => []
-            }.merge(group_config)
-            groups << @mymodel_group
-          end
-          @mymodel_group[:items] << {
-            boxLabel: c.to_s, 
-            name: "records", 
-            input_value: c.id, 
-            :field_label => "", 
-            :xtype => 'checkbox'
-          }.merge(item_config)
-          grp = configuration[:get_relation].call(c).id
+        configuration[:get_groups].call.each_with_index do |g, i|
+          items = configuration[:get_members].call(g).collect { |c|
+            {
+              boxLabel: c.to_s, 
+              name: "records", 
+              input_value: c.id, 
+              checked: selected_ids.include?(c.id),
+              :field_label => "", 
+              :xtype => 'checkbox'
+            }.merge(item_config)
+          }
+          groups << {
+            :xtype => 'checkboxgroup',
+            :fieldLabel => g.to_s,
+            :cls => (i % 2) == 0  ? 'x-check-group-alt' : '' ,
+            :columns => 7,
+            :items => items
+          }.merge(group_config)
         end
+        groups << { xtype: 'hidden', name: 'record_id', value: configuration[:record_id].to_i }
         super.merge(:items => groups)
       end
+
+      # TO DO
+      # figure out a way to make this work with the FormPanel model method
+      def the_record
+        find_record(configuration[:record_id])
+      end
+
+      def find_record(id)
+        Kernel.const_get(configuration[:record_model]).find(id)
+      end
+
+
+      endpoint :netzke_submit do |params|
+        netzke_submit(params)
+      end
+
+      def netzke_submit(params)
+        data = ActiveSupport::JSON.decode(params[:data])
+        selected_ids = data['records']
+        selected_ids.delete(false)
+        if configuration[:assign_selected].call(find_record(data['record_id']), selected_ids )
+          {:set_result => true}
+        else
+          # flash eventual errors
+          nu.errors.to_a.each do |msg|
+            flash :error => msg
+          end
+          {:netzke_feedback => @flash, :apply_form_errors => build_form_errors(record)}
+        end
+      end
+
     end
+
   end
 end
 
